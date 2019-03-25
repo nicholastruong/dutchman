7dccaa4870ccde1eff4418d77b27b4b8c8300597
Nicholas Truong <ngtruong@usc.edu>
2019-03-25 11:10:30 -0700

    moved day to game state

--- events/incoming/facilitator/send_advance.js
+++ events/incoming/facilitator/send_advance.js
@@ -1,22 +1,24 @@
 const eventID = 'facilitator next day';
-var day = 1;
 module.exports = function(socket, server, game, config){
 
 	socket.on("facilitator next day", function(socket){
 		let currentGame = game['games']['0'];
 		let players = currentGame['players'];
 
-		++day;
+
+		currentGame['day'] += 1;
+		console.log('YOO');
+		console.log(currentGame['day']);
 		//for each player, update resource count in game state
 		//query game state for resources and send to each socket
-		var weather = game.weather[day];
+		var weather = game.weather['day'];
 		for (socketID in players) {
 			
 			var newResources = players[socketID]['resources'];
 
 			console.log(newResources);
-			game.updateResources(0, socketID, day); //0 is gameID
-			server.trigger['server send updateDay'](socketID, newResources, weather, day);
+			game.updateResources(0, socketID, currentGame['day']); //0 is gameID
+			server.trigger['server send updateDay'](socketID, newResources, weather, currentGame['day']);
 
 		}
 
