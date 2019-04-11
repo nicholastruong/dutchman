ccecc05ef682d0fabbdda45fc37dfba2f426f110
NiwradMendyke <mendyke@usc.edu>
2019-04-10 23:31:45 -0700

    disables teamtrade button when not colocated

--- game.js
+++ game.js
@@ -372,8 +372,11 @@
 
 			for (var key in game.players) {
 				if (key == playerID) continue;
+				let otherLocation = game.players[key]['currentLocation'];
 
-				if (game.players[key]['currentLocation'] == currentLocation) {
+				if (otherLocation == currentLocation
+					|| (currentLocation == 12 && [21, 22].includes(otherLocation))
+						|| ([21, 22].includes(currentLocation) && otherLocation == 12)) {
 					colocatedPlayers.push({
 						playerName: game.players[key]['username']
 					});
